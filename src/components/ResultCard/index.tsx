1a355b68b8244d23208d90ae9daf4a4b99c3f443
hstt-coder <eduardolopes19@outlook.com>
2023-03-06 22:07:55 -0300

    Profile Search

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,18 +1,51 @@
-import './styles.css';
+import "./styles.css";
 
 type Props = {
-    title: string;
-    description: string;
+    avatar_url: string;
+    url: string;
+    followers: string;
+    location: string;
+    name: string;
 }
 
-const ResultCard = ( { title, description} : Props) => {
-
-    return (
-        <div className="result-container">
-            <h3 className="result-title">{title}</h3>
-            <p className="result-description">{description}</p>
+const ResultCard = ({ avatar_url, url, followers, location, name}: Props) => {
+  return (
+    <div className="result-container">
+      <div className="image-container">
+        <img
+          src={avatar_url}
+          alt=""
+        />
+      </div>
+      <div className="info-container">
+        <h3>Informações</h3>
+        <div className="info-item">
+          <p>
+            <strong>Perfil: </strong>
+            <a href={url}
+               className="profile-link">
+              {url}
+            </a>
+          </p>
         </div>
-    );
-}
+        <div className="info-item">
+          <p>
+            <strong>Seguidores:</strong> {followers}
+          </p>
+        </div>
+        <div className="info-item">
+          <p>
+            <strong>Localidade:</strong> {location}
+          </p>
+        </div>
+        <div className="info-item">
+          <p>
+            <strong>Nome:</strong> {name}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ResultCard;
--- src/pages/ProfileSearch/index.tsx
+++ src/pages/ProfileSearch/index.tsx
@@ -1,8 +1,76 @@
+import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
 import './styles.css';
+import axios from 'axios';
 
+type FormData = {
+  profile: string
+}
+
+type UserProfile = {
+  avatar_url: string;
+  url: string;
+  followers: string;
+  location: string;
+  name: string;
+}
+
+const BASE_URL = "https://api.github.com/users/";
 
 const ProfileSearch = () => {
-  return <h1>Profile</h1>
+
+  const [formData, setFormData] = useState<FormData>({profile: ''})
+  const [userProfile, setUserProfile] = useState<UserProfile>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .get(`${BASE_URL}${formData.profile}`)
+      .then(response => setUserProfile(response.data))
+      .catch(error => setUserProfile(undefined));
+
+  }
+
+
+  return (
+    <div className="profile-search-container">
+      <div className="container">
+        <div className="search-container">
+          <h1>Encontre um perfil Github</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-container">
+              <input 
+                type="text" 
+                className="search-input" 
+                name="profile"
+                value={formData.profile}
+                placeholder="Perfil"
+                onChange={handleChange}
+                />
+              <button type="submit" className="btn btn-primary search-button">Encontrar</button>
+            </div>
+          </form>
+        </div>
+        { userProfile &&
+              <ResultCard 
+                avatar_url={userProfile.avatar_url}
+                url={userProfile.url}
+                followers={userProfile.followers}
+                location={userProfile.location}
+                name={userProfile.name}
+            /> 
+          }
+       
+      </div>
+    </div>
+  )
 }
 
 export default ProfileSearch;
